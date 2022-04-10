f55294a87179da79fda300f16722f49b45158df0
guilin <2469671909@qq.com>
2022-04-10 09:08:00 +0800

    "去掉打印"

--- src/test/java/com/guilin/studycode/fileTest/O1_FileTest.java
+++ src/test/java/com/guilin/studycode/fileTest/O1_FileTest.java
@@ -192,7 +192,6 @@ public class O1_FileTest {
         if (!fileType.equals("xlsx")) {
             System.out.println("msg文件类型错误不是excel类型" + fileType);
         }
-        System.out.println("2222222222222");
     }
 
     /**
