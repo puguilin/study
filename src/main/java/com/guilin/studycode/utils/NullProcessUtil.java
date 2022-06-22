bc3bf8ad0c045d187a97b16bbecc08ba79f9d32f
guilin <2469671909@qq.com>
2022-06-22 16:38:50 +0800

    '用map来接受参数——添加或修改学生信息'

---
+++ src/main/java/com/guilin/studycode/utils/NullProcessUtil.java
@@ -0,0 +1,106 @@
+package com.guilin.studycode.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Title: NullProcessUtil
+ * Description: 处理NULL对象的工具类
+ */
+public class NullProcessUtil {
+	 	
+	/**
+	 * 对数据对象进行null检测，如果为null，则返回相应的字符串
+	 * @param obj 需要检测的数据对象
+	 * @param target 如果obj为null，需要返回的目的字符串
+	 * @return 经过空值处理后的字符串
+	 */
+	public static String nvlToString(Object obj,String target){
+		return obj==null?target:String.valueOf(obj);
+	}
+	
+	/**
+	 * 对Integer数据对象进行null检测，如果为null，则返回相应int值
+	 * @param obj 需要检测的数据对象
+	 * @param target 如果obj为null，需要返回的int型数值
+	 * @return 经过空值处理后的int型数值
+	 */
+	public static int nvlToInt(Integer obj,int target){
+		return obj==null?target:obj.intValue();
+	}
+	
+	/**
+	 * 对Long数据对象进行null检测，如果为null，则返回相应long值
+	 * @param obj 需要检测的数据对象
+	 * @param target 如果obj为null，需要返回的long型数值
+	 * @return 经过空值处理后的long型数值
+	 */
+	public static long nvlToLong(Long obj,long target){
+		return obj==null?target:obj.longValue();
+	}
+	
+	/**
+	 * 对Float数据对象进行null检测，如果为null，则返回相应float值
+	 * @param obj 需要检测的数据对象
+	 * @param target 如果obj为null，需要返回的float型数值
+	 * @return 经过空值处理后的float型数值
+	 */
+	public static float nvlToFloat(Float obj,float target){
+		return obj==null?target:obj.floatValue();
+	}
+	
+	/**
+	 * 对Double数据对象进行null检测，如果为null，则返回相应double值
+	 * @param obj 需要检测的数据对象
+	 * @param target 如果obj为null，需要返回的double型数值
+	 * @return 经过空值处理后的float型数值
+	 */
+	public static double nvlToDouble(Double obj,double target){
+		return obj==null?target:obj.doubleValue();
+	}
+	
+	/**
+	 * 对Date数据对象进行null检测，如果为null，则返回相应Date值
+	 * @param obj 需要检测的数据对象
+	 * @param target 如果obj为null，需要返回的Date
+	 * @return 经过空值处理后的Date型数值
+	 */
+	public static Date nvlToDate(Date obj,Date target){
+		return obj==null?target:obj;
+	}
+	
+	/**
+	 * 
+	 */
+	public static boolean isNull(String obj){
+		String reg = "^\\s*$";
+		return (obj==null || "null".equals(obj) || "".equals(obj) || obj.matches(reg))?true:false;
+	}
+	public static boolean isObjNull(Object obj){
+		String reg = "^\\s*$";
+		return (obj==null || "null".equals(obj) || "".equals(obj))?true:false;
+	}
+	
+	public static String valiadateParam(Map<String,String> map){
+		ObjectMapper objectMapper = new ObjectMapper();
+		String returnStr = "";
+		for(String name:map.keySet()){
+            if(NullProcessUtil.isNull(map.get(name))){
+				Map<String,String> out = new HashMap<String,String>();
+				out.put("code", "4444");
+				out.put("msg", name + " is required!");
+				try {
+					returnStr= objectMapper.writeValueAsString(out);
+				} catch (JsonProcessingException e) {
+					out.toString();
+				}
+				break;
+			}
+		}
+		return returnStr;
+	}
+}
